42b2b1df16a70974a0efefd1005bb4ab89f4c096
Scott <syhlion@gmail.com>
2019-02-27 11:54:15 +0800

    fix glua redis bug & update vendor

--- vendor/github.com/syhlion/glua-libs/redis/parse.go
+++ vendor/github.com/syhlion/glua-libs/redis/parse.go
@@ -9,19 +9,7 @@ import (
 func parseReply(reply interface{}, L *lua.LState) (lv lua.LValue, err error) {
 
 	switch vv := reply.(type) {
-	case int:
-		lv = lua.LNumber(vv)
-	case bool:
-		lv = lua.LBool(vv)
-	case float64:
-		lv = lua.LNumber(vv)
-	case nil:
-		lv = lua.LNil
-	case int64:
-		lv = lua.LNumber(vv)
-	case string:
-		lv = lua.LString(vv)
-	case byte:
+	case []byte:
 		lv = lua.LString(vv)
 	default:
 		log.Printf("[ERROR] unknown type (value: `%#v`, converted: `%#v`)\n", reply, vv)
@@ -39,7 +27,7 @@ func parseReplys(replys []interface{}, L *lua.LState) (*lua.LTable, error) {
 		case []byte:
 			rows.Append(lua.LString(string(v.([]byte))))
 		default:
-			log.Printf("[ERROR] unknown type (value: `%#v`, converted: `%#v`)\n", v, vv)
+			log.Printf("[ERROR] unknown replys type (value: `%#v`, converted: `%#v`)\n", v, vv)
 			rows.Append(lua.LNil)
 		}
 	}
